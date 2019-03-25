144d058249a7ef635f2c81fc39259fdd265e5629
Tansu Dasli <tansudasli@gmail.com>
2019-03-25 16:30:39 +0300

    uploaded result file

--- uploadToGCP.sh
+++ uploadToGCP.sh
@@ -9,3 +9,33 @@ docker cp wordcount_mapper.py e75aeac85461:/
 docker cp wordcount_reducer.py e75aeac85461:/
 docker cp testfile1 e75aeac85461:/
 docker cp testfile2 e75aeac85461:/
+
+#give permission
+chmod +x wordcount_mapper.py
+chmod +x wordcount_reducer.py
+
+#create folder on hadoop
+export HADOOP_USER_NAME=hdfs
+hdfs dfs -mkdir /user/cloudera/input
+hdfs dfs -put testfile1 /user/cloudera/input
+hdfs dfs -put testfile2 /user/cloudera/input
+
+#run hadoop
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+   -input /user/cloudera/input \
+   -output /user/cloudera/output_new \
+   -mapper /wordcount_mapper.py \
+   -reducer /wordcount_reducer.py
+
+hdfs dfs -ls /user/cloudera/output_new
+hdfs dfs -cat /user/cloudera/output_new/part-00000
+
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
+   -input /user/cloudera/input \
+   -output /user/cloudera/output_new_1 \
+   -mapper /wordcount_mapper.py \
+   -reducer /wordcount_reducer.py \
+   -numReduceTasks 0
+
+#merge files
+hdfs dfs -getmerge /user/cloudera/output_new_0/* wordcount_num0_output.txt  
