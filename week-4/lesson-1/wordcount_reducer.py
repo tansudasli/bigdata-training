f652c5ce0336877881b0339c1ee33cf388d3abe0
Tansu Dasli <tansudasli@Tansus-MacBook-Pro.local>
2019-03-29 19:29:32 +0300

    minor corrctions and debugging

--- week-4/lesson-1/wordcount_reducer.py
+++ week-4/lesson-1/wordcount_reducer.py
@@ -3,46 +3,31 @@
 # ---------------------------------------------------------------
 #This reducer code will input a line of text and 
 #    output <word, total-count>
-#
-# python3 wordcount_reduce.py "line\t1\nline\t1" or
-# echo "line\t1\nline\t1 " | python3 wordcount_reducer.py
+# cat ./input/test* | ./wordcount_mapper.py | sort | ./wordcount_reducer.py
 # ---------------------------------------------------------------
 import sys
 
-last_key      = None #initialize these variables
+last_key = None 
 running_total = 0
 
-# -----------------------------------
 # Loop thru file
-#  --------------------------------
 for input_line in sys.stdin:
     input_line = input_line.strip()
 
-    # --------------------------------
-    # Get Next Word    # --------------------------------
+    # Get Next Word  
     this_key, value = input_line.split("\t", 1)  
-        #the Hadoop default is tab separates key value
-        #the split command returns a list of strings, in this case into 2 variables
     value = int(value)           
-        #int() will convert a string to integer (this program does no error checking)
- 
-    # ---------------------------------
-    # Key Check part
-    #    if this current key is same 
-    #          as the last one Consolidate
-    #    otherwise  Emit
-    # ---------------------------------
-    if last_key == this_key:     #check if key has changed ('==' is # logical equalilty check
-        running_total += value   # add value to running total
 
+    # Key Check part
+    #    if this current key is same as the last one Consolidate
+    #    otherwise  Emit
+    if last_key == this_key:   
+        running_total += value   # add value to running total
     else:
-        if last_key:             # if this key that was just read in
-                                 #   is different, and the previous 
-                                 #   (ie last) key is not empy,
-                                 #   then output the previous <key running-count>
+        if last_key: 
             print( "{0}\t{1}".format(last_key, running_total) )
-                                 # hadoop expects tab(ie '\t') separation
-        running_total = value    # reset values
+
+        running_total = value  
         last_key = this_key
 
 if last_key == this_key:
