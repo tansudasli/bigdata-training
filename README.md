2764d4be3e5e644f987cb967dd336a74eef24c30
Tansu Dasli <tansudasli@Tansus-MacBook-Pro.local>
2019-03-30 13:22:31 +0300

    minor corrections

--- README.md
+++ README.md
@@ -2,43 +2,4 @@
 Big Data (Hadoop and Spark) Training by University of California San Diego on Coursera platform
 
 #some notes
-use uploadToClouderaStackonGCP files for detailed commands.
-
-#some hadoop commands 
-> hdfs dfs -put localFile /user/cloudera     {put a local file into hdfs file system}
-> pig -x mapreduce          {pig CLI}
-   grunt>  A = load ‘frpmHDFSFile’ using PigStorage(‘separator’);
-   grunt>  B = foreach A generate $0, $1;
-   grunt>  dump B;
-   grunt>  store B into ‘outputFile’;
-   grunt>  quit
-
-> hdfs dfs -ls /user/cloudera   {shows outputFile}
-
-      
-Hive -> data in hfs and hbase  
-   
-> hdfs dfs -put localFile /tmp          
-> beeline -u jdbc:hive2://  
-   Jdbc:hive2://>  create table tableName (field STRING, field_2 INT) \
-                            ROW FORMAT DELIMITED FIELDS TERMINATED BY ‘separator’  \
-                             STORED AS TEXTFILE;
-   Jdbc:hive2://>  LOAD DATA INPATH ‘tmp/fileName’ OVERWRITE \
-                           INTO TABLE tableName;
-   Jdbc:hive2://>  Select * from tableName;
-
-       
-> hbase shell   {key=value database columnar}
-   hbase(main)>  create ‘tableName’, {NAME=>’field’}, {NAME=>’field_2’}
-   hbase(main)>  put ‘tableName’, ‘r1’, ‘field’, ‘value’  
-   hbase(main)>  put ‘tableName’, ‘r1’, ‘field_2’, ‘value_2’ 
-   hbase(main)>  put ‘tableName’, ‘r2’, ‘field’, ‘value’ 
-   hbase(main)>  put ‘tableName’, ‘r2’, ‘field_2’, ‘value_2’
-   hbase(main)>  scan ’tableName’
-   hbase(main)>  scan ‘tableName’,  {COLUMNS=>’field_2}
-  
-   
-      
-      
-  
-   
+general hadoop commands can be found 'n some hadoop commands.txt. use uploadToClouderaStackonGCP files for detailed commands.
--- week-4/lesson-1/uploadToClouderaStackonGCP.sh
+++ week-4/lesson-1/uploadToClouderaStackonGCP.sh
@@ -4,15 +4,15 @@ gcloud compute scp $(pwd)/test* cloudera-lite-1:~
 #connectto GCP instance
 gcloud compute --project "cloudera-training-232215" ssh --zone "europe-west3-c" "cloudera-lite-1"
 
-#upload files to cloudera docker, run below commands indivudually in yor terminal
-docker cp wordcount_mapper.py e75aeac85461:/ 
-docker cp wordcount_reducer.py e75aeac85461:/
-docker cp testfile1 e75aeac85461:/
-docker cp testfile2 e75aeac85461:/
+#transfer codes from GCP instance to cloudera-stack-docker
+#start docker. copy to docker. give execution permission
+docker cp california-bigdata-training/ ee75aeac85461:/
+chmod +x mapper.py
+chmod +x reducer.py
 
 #give permission
-chmod +x wordcount_mapper.py
-chmod +x wordcount_reducer.py
+chmod +x mapper.py
+chmod +x reducer.py
 
 #create folder on hadoop
 export HADOOP_USER_NAME=hdfs
@@ -23,20 +23,23 @@ hdfs dfs -put testfile2 /user/cloudera/input
 #run hadoop
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
    -input /user/cloudera/input \
-   -output /user/cloudera/output_new \
-   -mapper /wordcount_mapper.py \
-   -reducer /wordcount_reducer.py
+   -output /user/cloudera/output \
+   -mapper /mapper.py \
+   -reducer /reducer.py
 
-hdfs dfs -ls /user/cloudera/output_new
-hdfs dfs -cat /user/cloudera/output_new/part-00000
+#to see results
+hdfs dfs -ls /user/cloudera/output
+hdfs dfs -cat /user/cloudera/output/part-00000
+hdfs dfs -cat /user/cloudera/output/*
 
-#onlu mapper results will be delivered
+#only mapper results will be delivered
+#set numReduceTasks=0 !, and change output folder
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
    -input /user/cloudera/input \
-   -output /user/cloudera/output \
-   -mapper /wordcount_mapper.py \
-   -reducer /wordcount_reducer.py \
+   -output /user/cloudera/output-2 \
+   -mapper /mapper.py \
+   -reducer /reducer.py \
    -numReduceTasks 0
 
-#merge files for uploading
-hdfs dfs -getmerge /user/cloudera/output_new_0/* wordcount_num0_output.txt  
+#merge files for uploading under GCP instance's /
+hdfs dfs -getmerge /user/cloudera/output-2/* wordcount_output.txt  
