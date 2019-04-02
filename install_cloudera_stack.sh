9399caee7bd481f397cb0b4a611abd15660b2a32
Tansu Dasli <tansudasli@gmail.com>
2019-04-02 20:56:37 +0300

    minor corrections on install script

--- install_cloudera_stack.sh
+++ install_cloudera_stack.sh
@@ -4,7 +4,7 @@
 # 2- git clone ....
 # 3- or, manually on default ssh screens of GCP
 
-echo "give run permisson fist w/ chmod +x install_cloudera_stack.sh, then run ./install_cloudera_stack.sh"
+echo "give run permisson fist w/ chmod +x california-bigdata-training/install_cloudera_stack.sh, then run ./california-bigdata-training/install_cloudera_stack.sh"
 
 #install docker
 sudo apt-get update -y
@@ -22,7 +22,9 @@ wget https://downloads.cloudera.com/demo_vm/docker/cloudera-quickstart-vm-5.13.0
 tar -xvf cloudera-quickstart-vm-5.13.0-0-beta-docker.tar.gz
 cd cloudera-quickstart-vm-5.13.0-0-beta-docker/
 sudo docker import cloudera-quickstart-vm-5.13.0-0-beta-docker.tar
-sudo docker run --hostname=quickstart.cloudera --privileged=true -tid -p 80:80  -p 11000:11000 -p 7180:7180 -p 50070:50070 -p 21050:21050 -p 18088:18088 -p 2181:2181 -p 8983:8983 -p 8984:8984 -p 8888:8888 e37 /usr/bin/docker-quickstart
+echo "change image id"
+IMAGE_ID=$(docker images -a --format "{{.ID}}" | head -1)
+sudo docker run --hostname=quickstart.cloudera --privileged=true -tid -p 80:80  -p 11000:11000 -p 7180:7180 -p 50070:50070 -p 21050:21050 -p 18088:18088 -p 2181:2181 -p 8983:8983 -p 8984:8984 -p 8888:8888 $IMAGE_ID /usr/bin/docker-quickstart
 
-echo "open above ports on GCP Firewall !!!"
+echo "open above ports on GCP Firewall for web UI access !!!"
 echo "docker attach and run inside docker instance /home/cloudera/cloudera-manager --express --force"
