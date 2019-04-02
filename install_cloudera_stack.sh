367a5d092f7f3d0b720d1cdaf321789b60fa8c64
Tansu Dasli <tansudasli@gmail.com>
2019-04-02 19:41:40 +0300

    install script updated

--- install_cloudera_stack.sh
+++ install_cloudera_stack.sh
@@ -1,13 +1,16 @@
 #create GCP instance on GCP console
-#upload this file to GCP instance 
-# 1- via gcloud instance scp
-# 2- git clone
-# 3- manually on default ssh screens of GCP
+#upload this file to GCP instance via
+# 1- gcloud compute scp ....
+# 2- git clone ....
+# 3- or, manually on default ssh screens of GCP
 
 #install docker
 sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+#restart terminal
